b356f215b0f033613b3ec6a5e312f6ae0d949f86
Debabrata Mondal <debabrata.js@protonmail.com>
2022-12-12 18:34:04 +0530

    update login styles

--- frostbyte/components/Navbar.js
+++ frostbyte/components/Navbar.js
@@ -4,7 +4,10 @@ import Link from "next/link"
 export default function Navbar() {
 
   return (
-    <div className="bg-zinc-800">
+    <div className="flex items-center justify-between bg-zinc-800 p-3">
+      <h1 className="text-zinc-200 ml-4 text-xl">
+        ❄️   Frostbyte
+      </h1>
       <Button />
     </div>
   )
@@ -20,14 +23,18 @@ const Button = () => {
 
   if (session) {
     return (
-      <button onClick={handleLogout}>
+      <button onClick={handleLogout}
+        className="rounded bg-zinc-700 py-1 px-4 text-zinc-300 mr-4 text-sm duration-150 hover:bg-zinc-600 border border-zinc-600"
+      >
         Logout
       </button>
     )
   } else {
     return (
-      <Link href="/login">
-        Login
+      <Link href="/login"
+        className="rounded bg-zinc-700 py-1 px-4 text-zinc-300 mr-4 text-sm duration-150 hover:bg-zinc-600 border border-zinc-600"
+      >
+        Sign in
       </Link>
     )
   }
