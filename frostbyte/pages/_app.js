4c8971d0e15cf584a66286cad8affae359d6d749
Debabrata Mondal <debabrata.js@protonmail.com>
2022-12-13 16:50:08 +0530

    use supabase auth helpers in index

--- frostbyte/pages/_app.js
+++ frostbyte/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
 import { SessionContextProvider } from "@supabase/auth-helpers-react"
 import { useState } from "react"
@@ -10,10 +11,17 @@ function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient()) 
 
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Navbar />
-      <Component {...pageProps} />
-    </SessionContextProvider>
+    <>
+      <Head>
+        <title>Frostbyte</title>
+        <meta name='description' content='Video transcoder for Supabase storage' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Navbar />
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </>
   )
 }
 
