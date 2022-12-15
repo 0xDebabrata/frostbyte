89bd2880ad550e0386c2bd0780000a8fc7ea68d9
Debabrata Mondal <debabrata.js@protonmail.com>
2022-12-15 17:27:34 +0530

    refactor

---
+++ frostbyte/components/ProjectDetails.js
@@ -0,0 +1,29 @@
+export default function ProjectDetails({ project }) {
+  return (
+    <div className="py-5 px-10 flex flex-col sm:flex-row items-start w-full bg-zinc-800 rounded">
+      <p className="w-[40%] text-zinc-200 text-sm pb-5 sm:pt-1">
+        General settings
+      </p>
+      <div className="w-full">
+        <label className="text-zinc-400 font-light text-sm">Project ID</label>
+        <input type='text' 
+          className="px-2 py-1 text-sm w-full mt-1 mb-2 w-full rounded bg-zinc-700 font-light text-zinc-300 border border-zinc-600 focus:outline-none"
+          value={project.id}
+          readOnly
+          /> 
+        <label className="text-zinc-400 font-light text-sm">Project Name</label>
+        <input type='text' 
+          className="px-2 py-1 text-sm w-full mt-1 mb-2 w-full rounded bg-zinc-700 font-light text-zinc-300 border border-zinc-600 focus:outline-none"
+          value={project.name}
+          readOnly
+          /> 
+        <label className="text-zinc-400 font-light text-sm">Frostbyte API Key</label>
+        <input type='text' 
+          className="px-2 py-1 text-sm w-full mt-1 mb-2 w-full rounded bg-zinc-700 font-light text-zinc-300 border border-zinc-600 focus:outline-none"
+          value={project.api_key}
+          readOnly
+          /> 
+      </div>
+    </div>
+  );
+}
--- frostbyte/pages/project/[id].js
+++ frostbyte/pages/project/[id].js
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react"
+import { useState } from "react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs"
 
+import ProjectDetails from "../../components/ProjectDetails";
+
 export default function Project({ project, tasks }) {
-  const user = useUser()
   const supabaseClient = useSupabaseClient()
+  
+  const [view, setView] = useState("details")
 
   const menu = [
-    { label: "Details", name: "details", active: true },
-    { label: "Tasks", name: "tasks", active: false },
+    { label: "Details", name: "details" },
+    { label: "Tasks", name: "tasks" },
   ]
 
   return (
@@ -19,39 +22,18 @@ export default function Project({ project, tasks }) {
             {project.name}
           </h2>
           {menu.map(menu => (
-            <div key={menu.name} className={`${menu.active ? "bg-zinc-700 " : ""} cursor-default py-1 px-4 mr-4 text-zinc-300 font-light rounded text-sm hover:bg-zinc-600 duration-150`}>
+            <div key={menu.name} className={`${menu.name === view ? "bg-zinc-700 " : ""} cursor-default py-1 px-4 mr-4 text-zinc-300 font-light rounded text-sm hover:bg-zinc-600 duration-150`}
+              onClick={() => setView(menu.name)}
+            >
               {menu.label}
             </div>
           ))}
         </div>
       </div>
+
       <div className="bg-zinc-900 min-h-[calc(100vh-122px)] ">
         <div className="max-w-[800px] mx-auto px-10 pt-5">
-          <div className="py-5 px-10 flex flex-col sm:flex-row items-start w-full bg-zinc-800 rounded">
-            <p className="w-[40%] text-zinc-200 text-sm pb-5 sm:pt-1">
-              General settings
-            </p>
-            <div className="w-full">
-              <label className="text-zinc-400 font-light text-sm">Project ID</label>
-              <input type='text' 
-                className="px-2 py-1 text-sm cursor-default w-full mt-1 mb-2 w-full rounded bg-zinc-700 font-light text-zinc-300 border border-zinc-600 focus:outline-none focus:border-zinc-500"
-                value={project.id}
-                readOnly
-                /> 
-              <label className="text-zinc-400 font-light text-sm">Project Name</label>
-              <input type='text' 
-                className="px-2 py-1 text-sm cursor-default w-full mt-1 mb-2 w-full rounded bg-zinc-700 font-light text-zinc-300 border border-zinc-600 focus:outline-none focus:border-zinc-500"
-                value={project.name}
-                readOnly
-                /> 
-              <label className="text-zinc-400 font-light text-sm">Frostbyte API Key</label>
-              <input type='text' 
-                className="px-2 py-1 text-sm cursor-default w-full mt-1 mb-2 w-full rounded bg-zinc-700 font-light text-zinc-300 border border-zinc-600 focus:outline-none focus:border-zinc-500"
-                value={project.api_key}
-                readOnly
-                /> 
-            </div>
-          </div>
+          { view === "details" && (<ProjectDetails project={project} />)}
         </div>
       </div>
     </div>
