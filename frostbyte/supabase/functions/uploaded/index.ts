a12b02f63e68642511b5d4f8b5a8348920c94810
Debabrata Mondal <debabrata.js@protonmail.com>
2022-12-16 09:20:19 +0530

    deploy edge functions

--- frostbyte/supabase/functions/uploaded/index.ts
+++ frostbyte/supabase/functions/uploaded/index.ts
@@ -1,25 +1,78 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { decode as hd } from "https://deno.land/std/encoding/hex.ts";
 
-console.log("Hello from Functions!")
+// Frostbyte supabase client
+const supabaseClient = createClient(
+  Deno.env.get("SUPABASE_URL"),
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"),
+)
 
 serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  const te=(s:string)=>new TextEncoder().encode(s),
+        td=(d:Uint8Array)=>new TextDecoder().decode(d);
+
+  const apiKey = req.headers.get("api-key")
+  const { bucket_id, filename, task_id } = await req.json()
+
+  // Fetch project details
+  const { data, error } = await supabaseClient
+    .from("projects")
+    .select("id, name, supabase_url, supabase_secret, iv")
+    .eq("api_key", apiKey)
+
+  if (error) {
+    console.error("Supabase query error: ", error)
+    return new Response(JSON.stringify({ error }), {
+      status: 500
+    })
   }
 
+  if (!data.length) {
+    return new Response("Unauthorized.", {
+      status: 401
+    })
+  }
+
+  // Decrypt secret key
+  const key = await crypto.subtle.importKey(
+    "raw",
+    hd(te(Deno.env.get("AES_KEY"))).buffer,
+    "AES-CBC",
+    true,
+    ["encrypt", "decrypt"],
+  );
+
+  const iv = hd(te(data[0].iv))
+
+  const decrypted = await crypto.subtle.decrypt(
+    {name: "AES-CBC", iv},
+    key,
+    hd(te(data[0].supabase_secret)),
+  );
+
+  const decryptedBytes = new Uint8Array(decrypted);
+  const secretKey = td(decryptedBytes);
+
+  // Get public URL for uploaded object
+  const client = createClient(data[0].supabase_url, secretKey)    // Supabase client with user's project admin privileges
+  const { data: signedUrl } = await client
+    .storage
+    .from(bucket_id)
+    .createSignedUrl(filename, 86400)
+
+  console.log(signedUrl)
+
   return new Response(
-    JSON.stringify(data),
+    JSON.stringify({
+      secretKey, signedUrl
+    }),
     { headers: { "Content-Type": "application/json" } },
   )
 })
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+
+
+// Deployment manual:
+// 1. Ensure pg_net extension is enabled
+// 2. Add postgres function and trigger
