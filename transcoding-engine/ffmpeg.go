8abb3c262fc60170bfac05a8aab80bd4a5e573bb
Debabrata Mondal <debabrata.js@protonmail.com>
2024-04-17 14:10:23 +0530

    add scaling argument function

--- transcoding-engine/ffmpeg.go
+++ transcoding-engine/ffmpeg.go
@@ -2,30 +2,45 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"time"
-    "log"
 
 	"gopkg.in/vansante/go-ffprobe.v2"
 )
 
 var videoCodecMap = map[string]string{
-    "unchanged": "copy",
+    "original": "copy",
     "h264": "libx264",
     "h265": "libx265",
+}
+
+var videoResolutions = map[string]int{
+    "4k": 4096,
+    "1080p": 1080,
+    "720p": 720,
+    "480p": 480,
+}
+
+func generateScaleFilter(
+    job *Job,
+    ffprobeOutput *ffprobe.ProbeData,
+) string {
+    inputVideoStream := ffprobeOutput.FirstVideoStream()
+    inputVideoWidth := inputVideoStream.Width
+    inputVideoHeight := inputVideoStream.Height
+    if job.resolution == "original" {
+        return fmt.Sprintf("scale=%d:%d", inputVideoWidth, inputVideoHeight);
+    }
+
+    return fmt.Sprintf("scale=-2:%d", videoResolutions[job.resolution])
 }
 
 func getTranscodingVideoAudioCodecs(
     inputFileName string,
     job *Job,
+    ffprobeOutput *ffprobe.ProbeData,
 ) (string, string) {
-    ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancelFn()
-
-    // Read input video and audio codecs
-    ffprobeOutput, err := ffprobe.ProbeURL(ctx, inputFileName)
-    if err != nil {
-        log.Panicf("Error probing video file: %v", err)
-    }
     inputVideoCodec := ffprobeOutput.FirstVideoStream().CodecName
     inputAudioCodec := ffprobeOutput.FirstAudioStream().CodecName
 
@@ -33,7 +48,7 @@ func getTranscodingVideoAudioCodecs(
     // Copy codec if file already has supported codecs. Prevents unnecessary
     // transcoding
     outputVideoCodec := "copy"
-    if job.videoCodec != "unchanged" && job.videoCodec != inputVideoCodec {
+    if job.videoCodec != "original" && job.videoCodec != inputVideoCodec {
         outputVideoCodec = videoCodecMap[job.videoCodec]
     }
     outputAudioCodec := "aac"
@@ -44,11 +59,26 @@ func getTranscodingVideoAudioCodecs(
     return outputVideoCodec, outputAudioCodec
 }
 
+func probeInputVideoData(
+    inputFileName string,
+) *ffprobe.ProbeData {
+    ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancelFn()
+
+    // Read input video and audio codecs
+    ffprobeOutput, err := ffprobe.ProbeURL(ctx, inputFileName)
+    if err != nil {
+        log.Panicf("Error probing video file: %v", err)
+    }
+    return ffprobeOutput
+}
+
 func processVideo(
     inputFileName string,
     outputFileName string,
     job *Job,
+    ffprobeOutput *ffprobe.ProbeData,
 ) {
-    videoCodecArg, audioCodecArg := getTranscodingVideoAudioCodecs(inputFileName, job)
+    videoCodecArg, audioCodecArg := getTranscodingVideoAudioCodecs(inputFileName, job, ffprobeOutput)
     log.Println(videoCodecArg, audioCodecArg)
 }
