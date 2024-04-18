ef01a4ab3cc5ac10a9856db5d8420205f3a8c509
Anish Basu <anishbasoup@gmail.com>
2024-04-18 18:19:55 +1000

    add login page

---
+++ web/src/app/login/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export default function Login() {
+  const supabase = createClientComponentClient();
+  async function signInWithGithub() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+
+    console.log(data);
+  }
+
+  return (
+    <div className="mx-auto font-mono flex min-h-screen flex-col items-center justify-between p-24">
+      <button
+        className="bg-indigo-400 w-64 p-2 text-white text-semibold rounded-md"
+        onClick={signInWithGithub}
+      >
+        Sign in with Github
+      </button>
+    </div>
+  );
+}
