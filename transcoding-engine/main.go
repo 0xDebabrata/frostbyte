0708e137f6e3ffcf13341f999a37ce384d8fa4e8
Debabrata Mondal <debabrata.js@protonmail.com>
2024-04-22 00:26:02 +0530

    remove debugging statements

--- transcoding-engine/main.go
+++ transcoding-engine/main.go
@@ -71,16 +71,13 @@ func main() {
 
             // Conversion process
             inputVideoData, err := probeInputVideoData(localFilePath)
-            log.Println("Video probed")
             if err != nil {
                 logErrorToSupabase(
                     &logUpdateParam,
                     fmt.Sprintf("Error probing file: %v", err),
                 )
-                log.Println("here")
                 break
             }
-            log.Println("here 2")
             processVideo(
                 localFilePath,
                 localOutputPath,
